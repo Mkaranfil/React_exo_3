23f253d17af37b490e03cb12209c2c22c2262665
Mustafa <startmk2020@gmail.com>
2021-01-14 16:07:54 +0100

    Modification du component App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-
+import img1 from './batman.jpg'
 
 function App() {
   return (
     <div className="App">
+      <h1 style={{color:"blue"}}>Exercie 3</h1>
+      <img src={img1} alt=""/>
+
     </div>
   );
 }
