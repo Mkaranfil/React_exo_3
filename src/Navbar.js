22452f404230ec4f956f47136d5946f196bab04e
Mustafa <startmk2020@gmail.com>
2021-01-14 16:14:02 +0100

    Fin exo 3

---
+++ src/Navbar.js
@@ -0,0 +1,19 @@
+
+
+
+function Navbar() {
+    return (
+      <div className="ElementParent">
+
+          <ul>
+              <li>Home</li>
+              <li>Contact</li>
+              <li>Portfolio</li>
+          </ul>
+
+      </div>
+    )
+  }
+
+
+  export default Navbar;
